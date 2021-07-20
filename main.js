d669799382691793930b4fd3a1cb05fde7f8782a
Prashant Tandan <pracant.tandon@gmail.com>
2021-07-20 19:54:30 +0545

    Added transactions and mining rewards

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '') {
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.nonce = 0;
         this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
     //method for implementing proof-of-work
@@ -30,21 +36,54 @@ class Block {
 class BlockChain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 4;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, '06/03/2021', "Genesis Block", "0");
+        return new Block('06/03/2021', "Genesis Block", "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        //Here, pending transactions will contain all the transactions that are on the array.
+        //In real bitCoin , the miner gets to choose which transactions he wants to include in the block.
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("Block successfully mined!");
+        this.chain.push(block);
+
+        //add the transaction for rewarding to the pending transactions list.
+        //From :system(so null), To:miner's address
+        this.pendingTransactions=[new Transaction(null, miningRewardAddress, this.miningReward)];
+
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance= 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+        return balance;
     }
 
     isChainValid() {
@@ -68,13 +107,19 @@ class BlockChain {
 //creating a new blockchain.
 let buttCoin = new BlockChain();
 
-buttCoin.addBlock(new Block(1, "06/03/2021", { amount: 4 }));
-buttCoin.addBlock(new Block(2, "06/03/2021", { amount: 6 }));
-console.log(JSON.stringify(buttCoin, null, 4));
-console.log("Is buttCoin valid? : " + buttCoin.isChainValid());
-console.log('----------------------------------------------------------')
-//tampering with the blockchain data (hash won't match).
-buttCoin.chain[1].data = { amount: 50 }
-console.log(JSON.stringify(buttCoin, null, 4));
-console.log("Is buttCoin valid? : " + buttCoin.isChainValid());
-console.log('----------------------------------------------------------')
+buttCoin.createTransaction(new Transaction('address1', 'address2', 300));
+buttCoin.createTransaction(new Transaction('address2', 'address1', 200));
+
+console.log('\n Starting the miner ...');
+buttCoin.minePendingTransactions('minerAdd');
+
+console.log("Miner's balance = ", buttCoin.getBalanceOfAddress('minerAdd'));
+console.log("Add1's balance = ", buttCoin.getBalanceOfAddress('address1'));
+console.log("Add2's balance = ", buttCoin.getBalanceOfAddress('address2'));
+
+
+
+console.log('\n Starting the miner again ...');
+buttCoin.minePendingTransactions('minerAdd');
+
+console.log("Miner's balance = ", buttCoin.getBalanceOfAddress('minerAdd'));
